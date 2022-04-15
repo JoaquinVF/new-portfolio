191261ea4c09bfbfa631027fa7c60ef147338fba
Joaquin Villanueva <74026054+JoaquinVF@users.noreply.github.com>
2022-04-15 00:36:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal(document.querySelector(".rv1"), { reset: true, duration: 1500});
+ScrollReveal().reveal(document.querySelector(".rv2"), { reset: true, duration: 1800});
+ScrollReveal().reveal(document.querySelector(".rv3"), { reset: true, duration: 1800});
+ScrollReveal().reveal(document.querySelector(".more-work"), { reset: true, duration: 1800});
